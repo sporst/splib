a64e4acdd2af115f45035ca5dfd08dabfe1421aa
Sebastian Porst <sp@porst.tv>
2011-06-11 19:49:03 -0700

    - Added the StringHelpers::join method to join string parts

--- src/tv/porst/splib/strings/StringHelpers.java
+++ src/tv/porst/splib/strings/StringHelpers.java
@@ -6,6 +6,32 @@ package tv.porst.splib.strings;
 public final class StringHelpers {
 
 	/**
+	 * Joins a bunch of strings into a single string.
+	 * 
+	 * @param parts The string parts to join.
+	 * @param delimiter The delimiter to add between strings.
+	 * 
+	 * @return The joined string.
+	 */
+	public static String join(final String[] parts, final String delimiter) {
+		final StringBuilder builder = new StringBuilder();
+
+		for (final String part : parts) {
+			if (part == parts[0] && (part == null || part.equals(""))) {
+				continue;
+			}
+
+			if (builder.length() != 0) {
+				builder.append(delimiter);
+			}
+
+			builder.append(part);
+		}
+
+		return builder.toString();
+	}
+
+	/**
 	 * Creates a new string by repeating a given string a number of times.
 	 * 
 	 * @param string The string to repeat.
